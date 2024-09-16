107ee8206518c5d113c97a404a8a48727206d6f5
Invdigitales <97065304+Invdigitales@users.noreply.github.com>
2024-09-16 15:03:20 -0300

    tres

--- main.js
+++ main.js
@@ -69,10 +69,22 @@ boton.addEventListener("click", () => {
       icon: 'success',
       html:
         'INGRESÁ' +
-        '<iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d13085.906183461464!2d-57.9292108!3d-34.9195836!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x95a2e61ce7c998a7%3A0x57bd083f7fde9725!2sAzul%20Multieventos!5e0!3m2!1ses-419!2sar!4v1721390975598!5m2!1ses-419!2sar" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>' +
+        '<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3283.0525061991025!2d-58.59435592674958!3d-34.628113398974115!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x95bcb87ab957e6a1%3A0x710fea4d81279bc1!2sEvaristo%20Carriego%201240%2C%20B1685FLJ%20El%20Palomar%2C%20Provincia%20de%20Buenos%20Aires!5e0!3m2!1ses-419!2sar!4v1726509459344!5m2!1ses-419!2sar" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>' +
         'Podes ampliar el mapa',
         
         
     })
   });
 
+  function exito(){
+    Swal.fire({
+        title: '<strong>VER LA UBICACION<u></u></strong>',
+        icon: 'success',
+        html:
+          'INGRESÁ' +
+          '<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3283.34032277854!2d-58.60049202529727!3d-34.62083915840079!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x95bcb8605d59831b%3A0xd5d29c444191a0fe!2sCap.%20Claudio%20Rosales%20853%2C%20B1684%20El%20Palomar%2C%20Provincia%20de%20Buenos%20Aires!5e0!3m2!1ses-419!2sar!4v1726509613992!5m2!1ses-419!2sar" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>' +
+          'Podes ampliar el mapa',
+          
+          
+      })
+}
